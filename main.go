94a9cfe5a5fd34806233224eadc0a2ad7d94b1d8
Rijenth <a_rijenth@laposte.net>
2024-07-02 11:30:10 +0200

    install dependancies and update dockerfile

--- main.go
+++ main.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello, World!",
+		})
+	})
+
+	router.Run(":8000")
 }
