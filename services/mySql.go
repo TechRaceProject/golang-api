6fc19f7f529c59fdb24237c7023cf65071bd68d0
Rijenth <a_rijenth@laposte.net>
2024-07-02 10:41:01 +0200

    fix: indentation

--- services/mySql.go
+++ services/mySql.go
@@ -31,17 +31,22 @@ func InitSqlConnection() (*gorm.DB, error) {
 	var err error
 
 	err = godotenv.Load()
+
 	if err != nil {
 		log.Fatal("Error loading .env file")
+
 		return nil, err
 	}
 
 	var dbUsername, dbPassword, dbHost, dbPort, dbName = getDBEnv()
 
 	dsn := dbUsername + ":" + dbPassword + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName + "?charset=utf8mb4&parseTime=True&loc=Local"
+
 	database, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
 	if err != nil {
 		log.Fatal("Error connecting to database: ", err)
+
 		return nil, err
 	}
 
