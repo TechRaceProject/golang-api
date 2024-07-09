cfaf14a11ed1ccc7dd3418c6c79702d9f3db8e4f
Rijenth <a_rijenth@laposte.net>
2024-07-09 14:02:19 +0200

    refacto: main function to improve readability

--- main.go
+++ main.go
@@ -6,33 +6,74 @@ import (
 	"api/src/services"
 	"fmt"
 	"log"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 func main() {
 	fmt.Println("Starting server on port 8000...")
 
+	database := initDatabase()
+
+	defer closeDatabaseConnection(database)
+
+	performMigrations(database)
+
+	initMQTT()
+
+	startWebServer()
+}
+
+func initDatabase() *gorm.DB {
 	database, err := services.InitSqlConnection()
+
 	if err != nil {
 		log.Fatal("An error occurred with the database connection: ", err)
 	}
 
+	fmt.Println("Database connection established.")
+
+	return database
+}
+
+func closeDatabaseConnection(database *gorm.DB) {
 	connection, err := database.DB()
+
 	if err != nil {
-		log.Fatal("An error occurred with the database connection: ", err)
+		log.Println("An error occurred while closing the database connection: ", err)
+		return
 	}
-	defer connection.Close()
+
+	connection.Close()
 
 	err = database.AutoMigrate(&models.User{}, &models.SensorData{}, &models.Vehicle{})
+
 	if err != nil {
 		log.Fatal("Error performing database migrations: ", err)
 	}
 
+	fmt.Println("Database migrations completed.")
+}
+
+func initMQTT() {
 	fmt.Println("Starting mqtt connection...")
+
 	client := services.InitMQTTClient("tcp://mosquitto:1883")
+
 	services.Subscribe(client, "esp32/#")
 
-	router := routes.SetupRouter()
+	fmt.Println("MQTT connection established and subscribed to topic.")
+}
+
+func startWebServer() {
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.Use(gin.Recovery())
+
+	router = routes.SetupRouter(router)
+
 	router.Run(":8000")
 
-	select {}
+	fmt.Println("Server started on port 8000.")
 }
