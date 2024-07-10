2ed453d0a96d1fc397794e36f4f5a1493285e73f
gaoubak <kader.bakayoko@yahoo.com>
2024-07-10 13:25:22 +0200

    :sparkles: Add new model Race , Fool , Model & use Model in other Model & add Username in user

--- src/models/userModel.go
+++ src/models/userModel.go
@@ -1,16 +1,9 @@
 package models
 
-import (
-	"time"
-
-	"gorm.io/gorm"
-)
-
 type User struct {
-	ID        uint `gorm:"primaryKey"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	DeletedAt gorm.DeletedAt `gorm:"index"`
-	Email     string         `gorm:"unique"`
-	Password  string
+	ID       uint   `gorm:"primaryKey"`
+	Email    string `gorm:"unique"`
+	Password string
+	Username string `json:"username"`
+	Model
 }
