5587ddbc1d05d7c0d60a5718506d3bdb3536904d
faust <80465380+gaoubak@users.noreply.github.com>
2024-07-11 16:22:34 +0200

    Revert "Add/controller/race"

--- src/routes/protected/setupProtectedRoutes.go
+++ src/routes/protected/setupProtectedRoutes.go
@@ -1,9 +1,7 @@
 package protected
 
 import (
-	controllers "api/src/controllers"
-	race_controller "api/src/controllers/race"
-	user_controller "api/src/controllers/user"
+	"api/src/controllers"
 	"api/src/middleware"
 
 	"github.com/gin-gonic/gin"
@@ -23,25 +21,4 @@ func SetupProtectedRoutes(routerGroup *gin.RouterGroup) {
 		vehicleGroup.DELETE("/:id", controllers.DeleteVehicle)
 
 	}
-
-	userGroup := routerGroup.Group("/user")
-	// USER ROUTES
-	{
-		userGroup.GET("/:userId", user_controller.UserHandler)
-		userGroup.PUT("/:userId", user_controller.UpdateUserHandler)
-		userGroup.GET("/", user_controller.GetAllUserHandler)
-		userGroup.DELETE("/:userId", user_controller.DeleteUserHandler)
-	}
-
-	raceGroup := routerGroup.Group("/race")
-
-	// RACE ROUTES
-	{
-		raceGroup.GET("/", race_controller.GetAllRaceHandler)
-		raceGroup.GET("/:raceId", race_controller.GetSingleRaceHandler)
-		raceGroup.POST("/", race_controller.CreateRaceHandler)
-		raceGroup.PATCH("/:raceId", race_controller.UpdateRaceHandler)
-		raceGroup.DELETE("/:raceId", race_controller.DeleteRaceHandler)
-	}
-
 }
