0ad71c11bf3270f57e8b4885ab3566b514b37df3
LFLeveneur <73178352+LFLeveneur@users.noreply.github.com>
2024-07-28 22:37:36 +0200

    :construction: [TEST] Vehicle - setup test helpers

---
+++ src/tests/setup_test_helpers.go
@@ -0,0 +1,34 @@
+package tests
+
+import (
+	"api/src/models"
+
+	"gorm.io/gorm"
+)
+
+func setupTestVehicle(db *gorm.DB) *models.Vehicle {
+	vehicle := &models.Vehicle{
+		Name:          "Test Vehicle",
+		BatteryLife:   100.0,
+		LineSensor1:   true,
+		LineSensor2:   false,
+		LineSensor3:   true,
+		Camera:        true,
+		SonarRange:    50.0,
+		WheelPower1:   90,
+		WheelPower2:   80,
+		WheelPower3:   70,
+		WheelPower4:   60,
+		LedColor:      "red",
+		DisplayPanel:  "LCD",
+		SpeakerStatus: true,
+		SoundPlaying:  "test sound",
+	}
+
+	err := db.Create(&vehicle).Error
+	if err != nil {
+		panic(err)
+	}
+
+	return vehicle
+}
