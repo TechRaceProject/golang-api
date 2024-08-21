32e6bcd4a7ff3eb27be24d04db00b7d7e3e59363
Fabrice Pivert <fpcomm@hotmail.com>
2024-08-21 12:01:14 +0200

    create race feature

--- src/validators/race/createRace.go
+++ src/validators/race/createRace.go
@@ -1,20 +1,44 @@
 package validators
 
-import "github.com/go-playground/validator/v10"
+import (
+	"errors"
+	"time"
+
+	"github.com/go-playground/validator/v10"
+)
 
 type CreateRaceValidator struct {
-	Duration          int    `json:"duration" validate:"required,gte=0"`
-	ElapsedTime       int    `json:"elapsed_time" validate:"required,gte=0"`
-	Laps              int    `json:"laps" validate:"required,gte=0"`
-	RaceType          string `json:"race_type" validate:"required,oneof=VS TIME_TRIAL"`
-	AverageSpeed      int    `json:"average_speed" validate:"required,gte=0"`
-	TotalFaults       int    `json:"total_faults" validate:"required,gte=0"`
-	EffectiveDuration int    `json:"effective_duration" validate:"required,gte=0"`
-	UserID            uint   `json:"user_id" validate:"required"`
-	VehicleID         uint   `json:"vehicle_id" validate:"required"`
+	StartTime            time.Time    `json:"start_time" validate:"required"`
+	EndTime             *time.Time   `json:"end_time" validate:"omitempty,gtefield=StartTime"`
+	NumberOfCollisions   uint8    `json:"number_of_collisions" validate:"required,gte=0"`
+	DistanceTravelled    int    `json:"distance_travelled" validate:"required,gte=0"`
+	AverageSpeed         int    `json:"average_speed" validate:"required,gte=0"`
+	OutOfParcours        uint8    `json:"out_of_parcours" validate:"required,gte=0"`
+	VehicleID         	 uint   `json:"vehicle_id" validate:"required"`
+	
+
 }
 
 func (c *CreateRaceValidator) Validate() error {
 	validate := validator.New()
 	return validate.Struct(c)
+}
+
+
+
+type UpdateRaceValidator struct {
+	StartTime time.Time  `json:"start_time"`
+	EndTime   *time.Time `json:"end_time" validate:"omitempty"`
+}
+
+func (u *UpdateRaceValidator) Validate() error {
+	validate := validator.New()
+
+	// Validation personnalisée: vérifier si EndTime n'est pas inférieur à StartTime
+	if u.EndTime != nil && u.EndTime.Before(u.StartTime) {
+		return errors.New("end_time cannot be before start_time")
+	}
+
+	// Si la validation personnalisée passe, alors on fait la validation standard des tags
+	return validate.Struct(u)
 }
