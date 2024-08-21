128249381658a5eae9dea9eab6527c534fbdc346
A_Rijenth <a_rijenth@laposte.net>
2024-08-21 11:24:26 +0200

    Refacto vehicle table (#21)
    * refacto: vehicle model
    
    * add: get and index vehicle routes
    
    * fix: error create vehicle when there is already the same one in db

--- src/models/vehicle.go
+++ src/models/vehicle.go
@@ -3,22 +3,10 @@ package models
 import "gorm.io/gorm"
 
 type Vehicle struct {
-	ID            uint    `gorm:"primaryKey"`
-	Name          string  `json:"vehicle_name"`
-	BatteryLife   float64 `json:"battery_life"`
-	LineSensor1   bool    `json:"line_sensor1"`
-	LineSensor2   bool    `json:"line_sensor2"`
-	LineSensor3   bool    `json:"line_sensor3"`
-	Camera        bool    `json:"camera"`
-	SonarRange    float64 `json:"sonar_range"`
-	WheelPower1   int     `json:"wheel_power1"`
-	WheelPower2   int     `json:"wheel_power2"`
-	WheelPower3   int     `json:"wheel_power3"`
-	WheelPower4   int     `json:"wheel_power4"`
-	LedColor      string  `json:"led_color"`
-	DisplayPanel  string  `json:"display_panel"`
-	SpeakerStatus bool    `json:"speaker_status"`
-	SoundPlaying  string  `json:"sound_playing"`
+	ID          uint   `gorm:"primaryKey"`
+	Name        string `json:"vehicle_name"`
+	IpAdress    string `json:"ip_adress"`
+	IsAvailable bool   `json:"is_available"`
 	Model
 }
 
--- src/tests/setup_test_helpers.go
+++ src/tests/setup_test_helpers.go
@@ -8,24 +8,13 @@ import (
 
 func SetupTestVehicle(db *gorm.DB) *models.Vehicle {
 	vehicle := &models.Vehicle{
-		Name:          "Test Vehicle",
-		BatteryLife:   100.0,
-		LineSensor1:   true,
-		LineSensor2:   false,
-		LineSensor3:   true,
-		Camera:        true,
-		SonarRange:    50.0,
-		WheelPower1:   90,
-		WheelPower2:   80,
-		WheelPower3:   70,
-		WheelPower4:   60,
-		LedColor:      "red",
-		DisplayPanel:  "LCD",
-		SpeakerStatus: true,
-		SoundPlaying:  "test sound",
+		Name:        "Test Vehicle",
+		IpAdress:    "1.1.1.1",
+		IsAvailable: true,
 	}
 
 	err := db.Create(&vehicle).Error
+
 	if err != nil {
 		panic(err)
 	}
