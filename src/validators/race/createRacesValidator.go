fb9cfdd62689794f730f5f05f04449300164bac6
Rijenth <a_rijenth@laposte.net>
2024-08-27 20:34:05 +0200

    hotfix: remove average_speed from create race validator

--- src/validators/race/createRacesValidator.go
+++ src/validators/race/createRacesValidator.go
@@ -7,13 +7,12 @@ import (
 )
 
 type CreateRaceValidator struct {
-	Name         string                 `json:"name" validate:"required"`
-	StartTime    attributes.CustomTime  `json:"start_time" validate:"required"`
-	EndTime      *attributes.CustomTime `json:"end_time" validate:"omitempty,gtefield=StartTime"`
-	Status       string                 `json:"status" validate:"required,oneof='not_started' 'in_progress' 'completed'"`
-	Type         string                 `json:"type" validate:"required,oneof='manual' 'auto'"`
-	VehicleID    uint                   `json:"vehicle_id" validate:"required"`
-	AverageSpeed float64                `json:"average_speed" validate:"required"`
+	Name      string                 `json:"name" validate:"required"`
+	StartTime attributes.CustomTime  `json:"start_time" validate:"required"`
+	EndTime   *attributes.CustomTime `json:"end_time" validate:"omitempty,gtefield=StartTime"`
+	Status    string                 `json:"status" validate:"required,oneof='not_started' 'in_progress' 'completed'"`
+	Type      string                 `json:"type" validate:"required,oneof='manual' 'auto'"`
+	VehicleID uint                   `json:"vehicle_id" validate:"required"`
 }
 
 func (c *CreateRaceValidator) Validate() error {
