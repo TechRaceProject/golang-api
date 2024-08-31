4d2342f53cfd060e6e1c6fd8f89972d61fa0e665
A_Rijenth <a_rijenth@laposte.net>
2024-08-31 14:03:19 +0200

    Refacto led principal color vehicle state relationship (#34)
    * feat: add list all vehicle state from a user
    
    * remove: VehicleState SecondaryLedColor relationship
    
    * refacto: handle new PrimaryLedColors relationship in update vehicle state
    
    * feat: add db transaction when updating a vehicle state

--- src/models/vehicleState.go
+++ src/models/vehicleState.go
@@ -4,21 +4,18 @@ import "encoding/json"
 
 type VehicleState struct {
 	ID        uint `gorm:"primaryKey" json:"id"`
-	VehicleID uint `json:"-"`
+	VehicleID uint `json:"vehicle_id"`
 	//Vehicle             Vehicle            `gorm:"foreignKey:VehicleID"`
-	Face                *uint8             `gorm:"not null" json:"face"`
-	LedAnimation        *uint8             `gorm:"not null" json:"led_animation"`
-	BuzzerAlarm         *uint8             `gorm:"not null" json:"buzzer_alarm"`
-	VideoActivated      *uint8             `gorm:"not null" json:"video_activated"`
-	PrimaryLedColorID   *uint              `json:"-"`
-	PrimaryLedColor     *PrimaryLedColor   `gorm:"foreignKey:PrimaryLedColorID"`
-	SecondaryLedColorID *uint              `json:"-"`
-	SecondaryLedColor   *SecondaryLedColor `gorm:"foreignKey:SecondaryLedColorID"`
-	BuzzerVariableID    *uint              `json:"-"`
-	BuzzerVariable      *BuzzerVariable    `gorm:"foreignKey:BuzzerVariableID"`
-	HeadAngleID         *uint              `json:"-"`
-	HeadAngle           *HeadAngle         `gorm:"foreignKey:HeadAngleID"`
-	UserID              uint               `json:"-"`
+	Face             *uint8            `gorm:"not null" json:"face"`
+	LedAnimation     *uint8            `gorm:"not null" json:"led_animation"`
+	BuzzerAlarm      *uint8            `gorm:"not null" json:"buzzer_alarm"`
+	VideoActivated   *uint8            `gorm:"not null" json:"video_activated"`
+	PrimaryLedColors []PrimaryLedColor `gorm:"foreignKey:VehicleStateID" json:"primary_led_colors"`
+	BuzzerVariableID *uint             `json:"-"`
+	BuzzerVariable   *BuzzerVariable   `gorm:"foreignKey:BuzzerVariableID"`
+	HeadAngleID      *uint             `json:"-"`
+	HeadAngle        *HeadAngle        `gorm:"foreignKey:HeadAngleID"`
+	UserID           uint              `json:"-"`
 	Model
 }
 
@@ -30,25 +27,23 @@ func (vehicleState *VehicleState) ToJson() (string, error) {
 	}
 
 	type attributes struct {
-		Face              *uint8      `json:"face"`
-		LedAnimation      *uint8      `json:"led_animation"`
-		BuzzerAlarm       *uint8      `json:"buzzer_alarm"`
-		VideoActivated    *uint8      `json:"video_activated"`
-		PrimaryLedColor   interface{} `json:"primary_led_color,omitempty"`
-		SecondaryLedColor interface{} `json:"secondary_led_color,omitempty"`
-		BuzzerVariable    interface{} `json:"buzzer_variable,omitempty"`
-		HeadAngle         interface{} `json:"head_angle,omitempty"`
+		Face             *uint8      `json:"face"`
+		LedAnimation     *uint8      `json:"led_animation"`
+		BuzzerAlarm      *uint8      `json:"buzzer_alarm"`
+		VideoActivated   *uint8      `json:"video_activated"`
+		PrimaryLedColors interface{} `json:"primary_led_colors,omitempty"`
+		BuzzerVariable   interface{} `json:"buzzer_variable,omitempty"`
+		HeadAngle        interface{} `json:"head_angle,omitempty"`
 	}
 
 	attr := attributes{
-		Face:              vehicleState.Face,
-		LedAnimation:      vehicleState.LedAnimation,
-		BuzzerAlarm:       vehicleState.BuzzerAlarm,
-		VideoActivated:    vehicleState.VideoActivated,
-		PrimaryLedColor:   vehicleState.PrimaryLedColor,
-		SecondaryLedColor: vehicleState.SecondaryLedColor,
-		BuzzerVariable:    vehicleState.BuzzerVariable,
-		HeadAngle:         vehicleState.HeadAngle,
+		Face:             vehicleState.Face,
+		LedAnimation:     vehicleState.LedAnimation,
+		BuzzerAlarm:      vehicleState.BuzzerAlarm,
+		VideoActivated:   vehicleState.VideoActivated,
+		PrimaryLedColors: vehicleState.PrimaryLedColors,
+		BuzzerVariable:   vehicleState.BuzzerVariable,
+		HeadAngle:        vehicleState.HeadAngle,
 	}
 
 	vsJson := vehicleStateJson{
