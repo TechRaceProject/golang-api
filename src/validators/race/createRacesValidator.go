47e53cafd04480ecfa236918aec765fd5a70fea1
A_Rijenth <a_rijenth@laposte.net>
2024-09-07 18:25:19 +0200

    refacto: better race status and start_time handling (#48)
    * refacto: set race status to `not_started` on create
    
    * refacto: set race `start_time` when race becomes `in_progress`

--- src/validators/race/createRacesValidator.go
+++ src/validators/race/createRacesValidator.go
@@ -10,7 +10,6 @@ type CreateRaceValidator struct {
 	Name      string                 `json:"name" validate:"required"`
 	StartTime *attributes.CustomTime `json:"start_time" validate:"omitempty"`
 	EndTime   *attributes.CustomTime `json:"end_time" validate:"omitempty,gtefield=StartTime"`
-	Status    string                 `json:"status" validate:"required,oneof='not_started' 'in_progress' 'completed'"`
 	Type      string                 `json:"type" validate:"required,oneof='manual' 'auto'"`
 	VehicleID uint                   `json:"vehicle_id" validate:"required"`
 }
